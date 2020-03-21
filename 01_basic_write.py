8eafd0e951460715406687857a2f48edf3ff9f96
ashik8 <ashikshaheen187@gmail.com>
2020-03-21 23:39:51 +0600

    Solved

---
+++ 01_basic_write.py
@@ -0,0 +1,7 @@
+handler = open("01_basic_write.txt", "w+")
+handler.write("1,'almasud','Abdullah Al Masud'\n"
+"2,'rimon','Rimon Ali'\n"
+"3,'niloy','Niloy Roy'\n"
+"4,'sourav','Sourav Dev Sharma'\n"
+"5,'sathi','Sathi Rani Roy'")
+handler.close()
